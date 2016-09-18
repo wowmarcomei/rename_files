7ecf4db9d18dea20ea64ccbe8aacbe2892ede170
wowmarcomei <wowmarcomei@gmail.com>
2016-09-18 14:23:39 +0630

    complete the basic features

--- source/rename.py
+++ source/rename.py
@@ -40,28 +40,58 @@ def FilesFilter(src,type):
         #筛选文件类型,并置于列表selectfilelist中
         for i in range(0,len(pathlist)):
             if (pathlist[i][-4:] == type):
-                selectFileList.append(pathlist[i])
+                # selectFileList.append(src)
+                selectFileList.append(src+'/'+pathlist[i])
 
         print("Selected files are: {}".format(selectFileList))
+
     return selectFileList
 
-FilesFilter(sourceDir,fileType['jpg'])
+def decodeMyFiles(file,filetype,myChar='Creation date',timePosition=8):
+    """解析音频或者照片文件,
 
+    :param file: 解析文件
+    :param filetype: 待解析文件的类型
+    :param myChar: 解析字符
+    :param timePosition: 解析出来的字符添加下划线的位置,如:20161212161616,变成20161212_161616
+    :return fileFinalTime: 解析出的文件创始时间
+    """
+    parserFile = parser.createParser(file) #解析文件
+    if not parserFile:
+        print("Unable to parse file!\n")
+        exit(1)
 
-# #如果没有目录则新建一个
-# if (not os.path.exists(targetdir)):
-#     try:
-#         os.mkdir(targetdir)
-#     except Exception:
-#         print('failed to create target directory.')
-# copycount = 0
-# totalcount = len(filelist)
-# for file in filelist:
-#     # 实现文件拷贝
-#     srcfile = io.open(file, "rb")
-#     # tarfile = io.open(targetdir+renameMyFiles(file,'jpg'), "wb")
-#     # tarfile.write(srcfile.read())
-#     # tarfile.close()
-#     srcfile.close()
-#     copycount += 1  # 计数器递增
-#     sys.stdout.write("\r%8d of %8d " % (copycount, totalcount))  # 打印进度
+    try:
+        metadataDecode = metadata.extractMetadata(parserFile)  # 获取文件的metadata
+    except ValueError:
+        print('Metadata extraction error.')
+        metadataDecode = None
+
+    if not metadataDecode:
+        print("Unable to extract metadata.")
+        exit(1)
+
+    myList = metadataDecode.exportPlaintext(line_prefix="") # 将文件的metadata转换为list,且将前缀设置为空
+
+    for i in range(1,len(myList)+1):
+        #如果字符串在列表中,则提取数字部分,即为文件创建时间
+        if myChar in myList[i-1]:
+            fileTime = re.sub(r"\D",'',myList[i-1])    #使用正则表达式将列表中的非数字元素剔除
+            a=list(fileTime)                           #将文件创建时间字符串转为列表list
+            a.insert(timePosition,'_')                 #将列表插入下划线分割date与time
+            fileFinalTime = "".join(a)                 #重新将列表转为字符串
+
+            print("The {0} is: {1}".format(myChar,fileFinalTime))
+            return fileFinalTime
+
+    print("Unable to get the creation time of {}\n".format(file))
+
+def RenameFiles(src,type,dst='../videosAndImages'):
+    files = FilesFilter(src,type)
+    for i in range(0,len(files)):
+        print("i is: {}".format(i))
+        a=decodeMyFiles(files[i],type)
+        os.rename(files[i], dst+'/'+a+type)
+
+RenameFiles(sourceDir,fileType['mov'])
+
